89b6fff868337c3fca293366b10a90fd61ad3332
bsjuilla <business060407@gmail.com>
2026-04-21 09:03:11 +0400

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,6 +3,6 @@ import { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: "*", allow: "/", disallow: "/search" },
-    sitemap: "https://copychars.com/sitemap.xml",
+    sitemap: "https://www.copychars.com/sitemap.xml",
   };
 }
