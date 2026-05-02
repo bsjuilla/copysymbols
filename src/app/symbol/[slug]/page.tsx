8a91aa8f874376875864bed2612f9f5da182765e
bsjuilla <business060407@gmail.com>
2026-05-02 10:43:18 +0400

    Update page.tsx

--- src/app/symbol/[slug]/page.tsx
+++ src/app/symbol/[slug]/page.tsx
@@ -5,6 +5,7 @@ import SymbolCopyButtons from "@/components/SymbolCopyButtons";
 import SymbolCard from "@/components/SymbolCard";
 import Link from "next/link";
 import type { Metadata } from "next";
+export const dynamic = "force-dynamic";
 
 interface Props { params: Promise<{ slug: string }> }
 
