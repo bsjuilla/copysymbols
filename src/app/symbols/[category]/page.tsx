461e47e5b6b8990af61bffa2cd76e3c02a02278a
bsjuilla <business060407@gmail.com>
2026-05-02 10:42:49 +0400

    Update page.tsx

--- src/app/symbols/[category]/page.tsx
+++ src/app/symbols/[category]/page.tsx
@@ -4,6 +4,7 @@ import CopyToast from "@/components/CopyToast";
 import SymbolCard from "@/components/SymbolCard";
 import Link from "next/link";
 import type { Metadata } from "next";
+export const dynamic = "force-dynamic";
 
 interface Props {
   params: Promise<{ category: string }>;
